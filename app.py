1264f758c5f449a9f88429519de9ba70013cbdb3
Tiago370 <tiagoamral23@gmail.com>
2026-01-21 15:26:40 -0300

    ADD tela de pagamento

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ def produtos():
         conn.commit();conn.close();return redirect("/produtos")
     produtos=c.execute("select * from produto").fetchall();conn.close()
     return render_template("produtos.html",produtos=produtos)
+def calcular_total():
+    return sum(i["sub_total"] for i in session["itens"])
 @app.route("/venda",methods=["GET","POST"])
 def venda():
     if "itens" not in session:session["itens"]=[]
@@ -37,13 +39,15 @@ def venda():
         sub_total = qtd * preco
         if p:session["itens"].append({"id":p[0],"desc":p[1],"preco":p[2],"qtd":qtd, "sub_total":sub_total});session.modified=True
 
-    total=sum(i["sub_total"] for i in session["itens"])
+    total = calcular_total()
     return render_template("venda.html",itens=session["itens"],total=total)
 @app.route("/finalizar")
 def finalizar():
     conn=db();c=conn.cursor()
     c.execute("insert into venda(data) values(?)",(datetime.now().isoformat(),));vid=c.lastrowid
     for i in session.get("itens",[]):c.execute("insert into item_venda(venda_id,produto_id,quantidade,preco_unit) values(?,?,?,?)",(vid,i["id"],1,i["preco"]))
-    conn.commit();conn.close();session["itens"]=[]
-    return redirect("/venda")
+    conn.commit();conn.close();
+    total = calcular_total()
+    session["itens"]=[]
+    return render_template("pagamento.html",total=total)
 app.run(debug=True)
