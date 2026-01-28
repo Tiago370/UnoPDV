f9c788f821c7ae97214c9bc0aa5686cfea232056
Tiago370 <tiagoamral23@gmail.com>
2026-01-28 12:14:32 -0300

    REF a funções da rota /packs

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask,render_template,request,redirect,session
 import sqlite3,os
 from datetime import datetime
 from utils import db, init_db
+from pack import pack_bp
 
 app=Flask(__name__)
 app.secret_key="pdv7"
 
 init_db()
+
+app.register_blueprint(pack_bp)
 
 @app.route("/")
 def index():return redirect("/venda")
@@ -109,51 +112,5 @@ def finalizar():
     total = calcular_total()
     session["itens"]=[]
     return render_template("pagamento.html",total=total)
-
-def buscar_pack_por_id(id):
-    conn=db();cur=conn.cursor()
-    cur.execute("SELECT id, codigo, nome, mnemonico, quantidade FROM pack WHERE id = ?", (id,))
-    pack = cur.fetchone()
-    conn.close()
-    return pack
-
-@app.route("/packs",methods=["GET","POST"])
-def packs():
-    conn=db();c=conn.cursor()
-    editar_id=request.args.get("editar_id")
-    pack_edicao=None
-    if editar_id:
-        pack_edicao=buscar_pack_por_id(editar_id)
-
-    filtros=[];valores=[]
-    if request.method=="POST":
-        acao=request.form["acao"]
-        codigo=request.form["codigo"]
-        nome=request.form["nome"]
-        mnemonico=request.form.get("mnemonico")
-        quantidade=request.form.get("quantidade")
-        id = request.form.get("id")
-        if acao=="salvar":
-            c.execute("select id from pack where id=?",(id,))
-            pack=c.fetchone()
-            if pack:
-                c.execute("update pack set codigo=?,nome=?,mnemonico=?,quantidade=? where id=?",(codigo,nome,mnemonico,quantidade,id))
-            else:
-                c.execute("insert into pack(codigo,nome,mnemonico,quantidade) values(?,?,?,?)",(codigo,nome,mnemonico,quantidade))
-            conn.commit();conn.close();return redirect("/packs")
-
-        elif acao=="consultar":
-            if codigo:
-                filtros.append("codigo LIKE ?");
-                valores.append(f"%{codigo}%")
-
-        elif acao=="deletar":
-            c.execute("delete from pack where id=?", (id,))
-            conn.commit();conn.close();return redirect("/packs")
-
-    where=" where "+" and ".join(filtros) if filtros else ""
-    packs=c.execute(f"select * from pack{where}",valores).fetchall()
-    conn.close()
-    return render_template("packs.html",packs=packs,pack_edicao=pack_edicao)
 
 app.run(debug=True)
